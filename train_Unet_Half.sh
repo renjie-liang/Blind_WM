9a7a1c09355987cc39d9e2f5142b1522c5cb4cb7
rj <allen_rj@163.com>
2019-09-27 23:31:43 +0800

    push new

---
+++ train_Unet_Half.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES='0' python3 -u main.py  \
+	--cfg configs/U_NET_Half.yaml  \
+	--bs 20  \
+	--nw 4  \
+	--name test_U_NET_Half \
+
---
+++ train_a39_Flatten.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES='2' python3 -u main.py  \
+	--cfg configs/DeConv_WMFlatten.yaml  \
+	--bs 20  \
+	--nw 4  \
+	--name a39_7_Flatten \
+
